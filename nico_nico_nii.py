4d0887afd5ef1b8bb99a0042b749a6d166f1795d
Evgeny <evgeny.singapore@gmail.com>
2018-05-27 21:42:56 +0800

    The last commit. I am done

--- nico_nico_nii.py
+++ nico_nico_nii.py
@@ -5,8 +5,19 @@ class repeatExtensionError(Exception):
 		self.message='You have attempted to use the same file extension more than once: '+ext
 	def __str__(self):
 		return self.message
-    
 
+def yee_files(self_name,storage_name,over_exts,over_paths):
+	dir=os.walk('.')
+	for root, dirs, files in dir:
+		for file in files:
+			if os.path.abspath(root+'/'+file)==self_name or os.path.abspath(root)==storage_name:
+				continue
+			file_arr=file.split('.')
+			if len(file_arr)!=2:
+				continue
+			if file_arr[1] in over_exts:
+				yield [root+'/'+file,over_paths[over_exts.index(file_arr[1])]]
+		
 def main():
 	storage_name=os.path.abspath('./overwrite_data')
 	self_name=os.path.abspath(sys.argv[0])
@@ -27,25 +38,17 @@ def main():
 			seen_exts.append(ext)
 		else:
 			raise repeatExtensionError('.'+ext)
-	dir=os.walk('.')
-	requires_assistance={}
-	for root, dirs, files in dir:
-		for file in files:
-			if os.path.abspath(root+'/'+file)==self_name or os.path.abspath(root)==storage_name:
-				continue
-			file_arr=file.split('.')
-			if len(file_arr)!=2:
-				continue
-			if file_arr[1] in over_exts:
-				requires_assistance[root+'/'+file]=over_paths[over_exts.index(file_arr[1])]
-	
-	for assist in requires_assistance:
-		file=open(requires_assistance[assist],'rb')
+
+	i=0
+	for assist in yee_files(self_name,storage_name,over_exts,over_paths):
+		i+=1
+		file=open(assist[1],'rb')
 		data=file.read()
 		file.close()
-		file=open(assist,'wb')
+		file=open(assist[0],'wb')
 		file.write(data)
 		file.close
+		print(str(i)+' files proccessed.', end="\r")
 	
 if __name__=="__main__":
 	main()
